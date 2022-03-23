753aafb05cb182e9e761bf49e4f5e2ea26f94106
jonathanGF <jonathanuri123@gmail.com>
2022-03-22 19:03:30 -0600

    Implementación de intercalación en polifase

--- Proyecto 1/src/Polifase/ordenamientoPol.java
+++ Proyecto 1/src/Polifase/ordenamientoPol.java
@@ -18,8 +18,31 @@ public class ordenamientoPol extends Polifase{
         int[] arregloordenado;
         arregloordenado = orden.bubbleSort(arrNumeros);
         System.out.println(Arrays.toString(arregloordenado));
-        }   
-      
+        }
+    
+    public void intercalacion(int aux1[], int aux2[]){//Primera implememtacion aun falta cabiarlo para strigs
+        int i,j,k;
+        
+        int arregloFinal[]=new int[aux1.length+aux2.length];
+        
+        for(i=j=k=0;i<aux1.length && j<aux2.length;k++){
+            if(aux1[i]<aux2[j]){
+                arregloFinal[k]=aux1[i];
+                i++;
+            }else{
+                arregloFinal[k]=aux2[j];
+                j++;
+            }
+        }
+        for(;i<aux1.length;i++,k++){//para poner elemetos que no tengan con quien comparar 
+            arregloFinal[k]=aux1[i];
+        }
+        for(;i<aux1.length;j++,k++){//para poner elemetos que no tengan con quien comparar 
+            arregloFinal[k]=aux2[j];
+        }
+        System.out.println(Arrays.toString(arregloFinal)); 
+    }
+        
     }
     
     
