3f8e82cc08b1842c5522082af2848fa9b680bb1e
adair86 <bgla1981@gmail.com>
2022-03-28 23:08:53 -0600

    La parte que sortea los bloques

--- Proyecto 1/src/Polifase/ordenamientoPol.java
+++ Proyecto 1/src/Polifase/ordenamientoPol.java
@@ -7,49 +7,66 @@ import java.util.List;
 import proyecto1.*;
 
 public class ordenamientoPol extends Polifase{
-    List<String[]> f0=new LinkedList<>();
-    public void fase1(String texto){
-       
-       String arrNombres[]=texto.split(",");
-       int arrNumeros[]=new int[arrNombres.length];
-        for (int i = 0; i < arrNombres.length-1; i++) {
-            arrNumeros[i]=Integer.parseInt(arrNombres[i]);
+    List<String[]> f0 = new LinkedList<>();
+
+    public void fase1(String texto) {
+
+        String arrNombres[] = texto.split(",");
+        int arrNumeros[] = new int[arrNombres.length];
+        for (int i = 0; i < arrNombres.length - 1; i++) {
+            arrNumeros[i] = Integer.parseInt(arrNombres[i]);
         }
-        }
+    }
+    int aux1[][], aux2[][];
     
-    
-    
-    public void intercalacion(int aux1[], int aux2[]){//Primera implememtacion aun falta cabiarlo para strigs
-        int i,j,k;
+    public void bloques(int arrNumeros[], int aux1[][], int aux2[][]){
         
-        int arregloFinal[ ]=new int[aux1.length+aux2.length];
-        
-        for(i=j=k=0;i<aux1.length && j<aux2.length;k++){
-            if(aux1[i]>aux2[j]){
-                arregloFinal[k]=aux1[i];
-                i++;
+        int cont = 0, x = 0, y=0;
+        boolean z = true;
+        for(int i = 0; i< arrNumeros.length; i++){
+            cont ++;
+            if(z== true){
+                aux1[x][i] = arrNumeros[i];
             }else{
-                arregloFinal[k]=aux2[j];
+                aux2[y][i] = arrNumeros[i];
+            }
+            
+            if(cont == 4){
+                z = false;
+                x ++;
+            }
+            if(cont == 8){
+                z = true;
+                y ++;
+                cont = 0;
+            }
+        }
+    }
+
+    public void intercalacion(int aux1[], int aux2[]) {//Primera implememtacion aun falta cabiarlo para strigs
+        int i, j, k;
+
+        int arregloFinal[] = new int[aux1.length + aux2.length];
+
+        for (i = j = k = 0; i < aux1.length && j < aux2.length; k++) {
+            if (aux1[i] > aux2[j]) {
+                arregloFinal[k] = aux1[i];
+                i++;
+            } else {
+                arregloFinal[k] = aux2[j];
                 j++;
             }
         }
-        for(;i<aux1.length;i++,k++){//para poner elemetos que no tengan con quien comparar 
-            arregloFinal[k]=aux1[i];
+        for (; i < aux1.length; i++, k++) {//para poner elemetos que no tengan con quien comparar 
+            arregloFinal[k] = aux1[i];
         }
-        for(;i<aux1.length;j++,k++){//para poner elemetos que no tengan con quien comparar 
-            arregloFinal[k]=aux2[j];
+        for (; i < aux1.length; j++, k++) {//para poner elemetos que no tengan con quien comparar 
+            arregloFinal[k] = aux2[j];
         }
-        System.out.println(Arrays.toString(arregloFinal)); 
+        System.out.println(Arrays.toString(arregloFinal));
     }
-        
-    }
-    
-    
-    
-    
-    
 
-  
+}
 
     
     
